0a34af5ea37f5ce36c7f74eec8110e136ef21464
ashu1909 <31014069+ashu1909@users.noreply.github.com>
2017-08-20 14:53:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article1',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','article1.html'));
+    res.sendFile(path.join(__dirname,'ui','article1.html'));
     
 })
 
